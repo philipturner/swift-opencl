890ac3c72ec9f06f412fd0b637ae70d9ca062d86
Philip Turner <philipturner.AR@gmail.com>
2022-04-26 16:10:03 -0400

    Beginnings of CLDevice

--- Sources/SwiftOpenCL/CLDevice.swift
+++ Sources/SwiftOpenCL/CLDevice.swift
@@ -5,4 +5,20 @@
 //  Created by Philip Turner on 4/26/22.
 //
 
-import Foundation
+import COpenCL
+
+public class CLDevice {
+  private static var default_initialized: Bool = false
+  private static var default_: CLDevice? = nil
+  private static var default_error_: Int32 = 0
+  
+  // line 2143: declare Device::makeDefault
+  // line 3211: implement Device::makeDefault
+  private static func makeDefault() {
+    
+  }
+  
+  private static func makeDefaultProvided(_ p: CLDevice) {
+    default_ = p
+  }
+}
