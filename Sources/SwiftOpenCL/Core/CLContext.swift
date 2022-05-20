83698db4956692284557cfa05624a09e25870611
Philip Turner <philipturner.AR@gmail.com>
2022-05-19 21:10:06 -0400

    commit changes

--- Sources/SwiftOpenCL/Core/CLContext.swift
+++ Sources/SwiftOpenCL/Core/CLContext.swift
@@ -24,5 +24,47 @@ public struct CLContext: CLReferenceCountable {
 
   static func release(_ object: OpaquePointer) -> Int32 {
     clReleaseContext(object)
-  }  
+  }
+  
+  static var defaultContext: CLContext? = {
+    #if !canImport(Darwin)
+    guard let p = CLPlatform.defaultPlatform else {
+      return nil
+    }
+    let defaultPlatform = p.platformID
+    return withUnsafeTemporaryAllocation(
+      of: cl_context_properties.self, capacity: 3
+    ) { properties in
+      properties[0] = Int(CL_CONTEXT_PLATFORM)
+      properties[1] = Int(bitPattern: defaultPlatform)
+      properties[2] = 0
+//      default_ = Context(
+//        CL_DEVICE_TYPE_DEFAULT,
+//        properties,
+//        NULL,
+//        NULL,
+//        &default_error_);
+      return nil
+    }
+    #else
+//    default_ = Context(
+//      CL_DEVICE_TYPE_DEFAULT,
+//      properties,
+//      NULL,
+//      NULL,
+//      &default_error_);
+    return nil
+    #endif
+  }()
+  
+  public init?(
+    devices: [CLDevice],
+    properties: UnsafeMutablePointer<cl_context_properties>? = nil,
+    notifyFptr: ((
+      UnsafePointer<Int8>, UnsafeRawPointer, Int, UnsafeMutableRawPointer) -> Void)? = nil,
+    data: UnsafeMutableRawPointer? = nil,
+    err: UnsafeMutablePointer<Int32>? = nil
+  ) {
+    fatalError()
+  }
 }
